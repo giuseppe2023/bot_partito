f4789263ed04f758f1d7aa851a6caab2aa5f5bec
giuseppe2023 <129619950+giuseppe2023@users.noreply.github.com>
2026-04-25 11:43:28 +0200

    Update embed description for membership instructions

--- commands/panel-tesserati.js
+++ commands/panel-tesserati.js
@@ -25,14 +25,14 @@ module.exports = {
 
         const embed = new EmbedBuilder()
             .setTitle('**TESSERAMENTO - INSIEME PER ROMA**')
-            .setDescription('Cari concittadini, tesserarsi a Insieme per Roma significa scegliere di esserci davvero. Non solo sostenere un’idea di città, ma contribuire in prima persona a costruirla, portando valore, partecipazione e visione. Con le sfide che ci attendono, ogni adesione conta: entra anche tu e diventa parte del cambiamento.
+            .setDescription(`Cari concittadini, tesserarsi a Insieme per Roma significa scegliere di esserci davvero. Non solo sostenere un’idea di città, ma contribuire in prima persona a costruirla, portando valore, partecipazione e visione. Con le sfide che ci attendono, ogni adesione conta: entra anche tu e diventa parte del cambiamento.
 
 __Istruzioni per il Tesseramento:__
 
 *Per completare il tesseramento è sufficiente premere il pulsante sottostante e compilare tutti i campi richiesti con attenzione.* Una volta inviata la richiesta, il nostro staff provvederà a verificarla: riceverete l’esito entro 24 ore.
 __Vi invitiamo a inserire informazioni corrette per evitare ritardi nella procedura.__
 
-**Entra anche tu a far parte di Insieme per Roma.**')
+**Entra anche tu a far parte di Insieme per Roma.**`)
             .setFooter({ text: 'Il tesseramento è gratuito e aperto a tutti!' })
             .setThumbnail(`${interaction.guild.iconURL()}`)
             .setColor('Blue');
